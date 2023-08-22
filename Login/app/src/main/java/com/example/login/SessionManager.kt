383a3b523c95cb507f8d779b94a8f9f99e04f272
Martin Andres Medina Armijos <84428426+Martin0017@users.noreply.github.com>
2023-08-21 22:44:49 -0500

    Merge feature into main

--- Login/app/src/main/java/com/example/login/SessionManager.kt
+++
@@ -1,5 +0,0 @@
-package com.example.login
-
-object SessionManager {
-    var mySessionData: String? = null
-}
--- Login/app/src/main/java/com/example/login/UserTemporalData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.login
-
-object UserTemporalData {
-    var id_user: String? = null
-    var nombre_user: String? = null
-    var apellido_user: String? = null
-    var fecha_nacimiento_user: String? = null
-    var genero_user: String? = null
-}
